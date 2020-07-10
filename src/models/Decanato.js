cee315fca0f91744454f3e48d93ce0b822dc6be4
ajav06 <albert06av@hotmail.com>
2020-07-10 08:34:23 -0400

    despliegue

--- src/models/Decanato.js
+++
@@ -1,9 +0,0 @@
-/* MODELO DE DATOS DE Decanato */
-export default class Decanato {
-    constructor(codigo, nombre, siglas, estatus='A'){
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.siglas = siglas;
-        this.estatus = estatus;
-    }
-}
